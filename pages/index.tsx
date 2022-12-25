b7840174abdf5a877b183c8c22cfc07a04b3a940
Umair <umairsadaqat@invotyx.co.uk>
2022-12-25 23:14:22 +0500

    Revert "Update index.tsx"
    This reverts commit c0897e96bb701937b961f37b70633118e93f5e9b.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
-import * as React from "react";
-import About from "./about";
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '../src/Link';
+import ProTip from '../src/ProTip';
+import Copyright from '../src/Copyright';
 
 export default function Home() {
   return (
-    <>
-      <h2>heelo world</h2>
-      <About />
-    </>
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          MUI v5 + Next.js with TypeScript example
+        </Typography>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
