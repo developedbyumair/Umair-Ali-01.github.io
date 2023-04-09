7bf30cfcfc38a8842448ed23d6d8bdf8ba6d5a94
Umair-Ali-01 <umairsadaqat@invotyx.co.uk>
2023-04-09 23:33:59 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "../src/Link";
+import ProTip from "../src/ProTip";
+import Copyright from "../src/Copyright";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,15 +13,20 @@ export default function Home() {
       <Box
         sx={{
           my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <Typography variant="h4" component="h1" gutterBottom>
           MUI v5 + Next.js with TypeScript example
         </Typography>
+        <Typography variant="h4" gutterBottom>
+          Image testing
+        </Typography>
+        {/* @ts-ignore */}
+        <Image src="/favicon.ico" width={760} height={760} />
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
