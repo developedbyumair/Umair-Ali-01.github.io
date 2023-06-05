adcd5a3df5b22c4f8e1634f6863c8558228e262d
Umair-Ali-01 <umairsadaqat@invotyx.co.uk>
2023-06-05 23:51:03 +0500

    Revert "testing"
    This reverts commit 395fe2ecf84723d9a1e39eb05823e8d2195ac46b.

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,13 @@
-// import { Roboto } from '@next/font/google';
+import { Roboto } from '@next/font/google';
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
-// export const roboto = Roboto({
-//   weight: ['300', '400', '500', '700'],
-//   subsets: ['latin'],
-//   display: 'swap',
-//   fallback: ['Helvetica', 'Arial', 'sans-serif'],
-// });
+export const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
 
 // Create a theme instance.
 const theme = createTheme({
@@ -23,7 +23,7 @@ const theme = createTheme({
     },
   },
   typography: {
-    // fontFamily: roboto.style.fontFamily,
+    fontFamily: roboto.style.fontFamily,
   },
 });
 
