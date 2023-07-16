c0ab6c14219e49e8e32e569a97302f9f7ee55cc0
Umair-Ali-01 <umairsadaqat@invotyx.co.uk>
2023-07-16 19:51:17 +0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,34 @@
+import React from "react";
+
 const Home = () => {
   return (
     <section id="home" className="home-section">
-      <div className="effect-1">
-        <img src="assets/img/effect-1.svg" className="svg" alt="image" />
-      </div>
       <div className="effect-2">
         <img src="assets/img/effect-2.svg" className="svg" alt="image" />
       </div>
       <div className="container">
         <div className="row min-vh-100 align-items-center">
-          <div className="col-lg-6 pe-xl-5 py-5">
+          <div className="col-lg-6 py-5">
             <div className="home-intro one-page-nav">
+              <div className="effect-1">
+                <img
+                  src="assets/img/effect-1.svg"
+                  className="svg"
+                  alt="image"
+                />
+              </div>
               <h6>
                 <span>Hello.</span>
               </h6>
               <h1>
-                We Have <br />
-                Design
+                Transforming <br />
+                Web Visions into
                 <br />
-                Experience
+                User-Friendly Realities
               </h1>
               <p>
-                I design and develop services for customers of all sizes,
-                specializing in creating stylish, modern websites, web services
-                and online stores.
+                I am committed to delivering web solutions that elevate users
+                online presence and connect with audience effectively.
               </p>
               <div className="btn-bar">
                 <a className="px-btn px-btn-theme" href="#contactus">
@@ -33,7 +38,7 @@ const Home = () => {
             </div>
           </div>
           <div className="col-lg-6">
-            <div className="home-image">
+            <div className="d-flex justify-content-lg-end">
               <img src="assets/img/Banner.jpg" alt="Banner" />
             </div>
           </div>
