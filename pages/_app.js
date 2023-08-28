47e8076ae7480e9a9200a72c507af9cd36d6ffae
developedbyumair <umairsadaqat@invotyx.co.uk>
2023-08-28 15:16:50 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,14 @@ import "../styles/globals.css";
 import { Fragment, useEffect, useState } from "react";
 import { ThemeProvider } from "react-bootstrap";
 import "font-awesome/css/font-awesome.min.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   const [loader, setLoader] = useState(true);
   useEffect(() => {
     setTimeout(() => {
       setLoader(false);
-    }, 1500);
+    }, 300);
   }, []);
 
   return (
@@ -17,7 +18,13 @@ export default function App({ Component, pageProps }) {
       breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
       minBreakpoint="xxs"
     >
-      {/* {loader && <Preloader />} */}
+      <Head>
+        <title>Umair Ali | Portfolio</title>
+        <meta name="description" content="Umair Ali | Portfolio" />
+        <meta name="keywords" content="Umair Ali | Portfolio" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      {loader && <Preloader />}
       <Component {...pageProps} />
     </ThemeProvider>
   );
