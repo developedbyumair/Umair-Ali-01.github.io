6bf1e23b8e081c4f5ba775187b31ac8fbb40c617
developedbyumair <umairsadaqat@invotyx.co.uk>
2023-08-28 15:22:45 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,20 +5,14 @@ const Footer = () => {
         <div className="row align-items-center">
           <div className="col-md-6 py-2">
             <div className="nav justify-content-center justify-content-md-start">
-              <a href="#">
-                <i className="fab fa-facebook-f" />
+              <a href="https://github.com/developedbyumair" target="_blank">
+                <i className="fab fa-github" />
               </a>
-              <a href="#">
+              <a href="https://twitter.com/DevByUmair" target="_blank">
                 <i className="fab fa-twitter" />
               </a>
-              <a href="#">
-                <i className="fab fa-instagram" />
-              </a>
-              <a href="#">
+              <a href="https://www.linkedin.com/in/umairali5/" target="_blank">
                 <i className="fab fa-linkedin-in" />
-              </a>
-              <a href="#">
-                <i className="fab fa-pinterest" />
               </a>
             </div>
           </div>
