3e6e73221797f83e701a54a9e5bc90f70ddd1b8b
Umair Ali <umairsadaqat@invotyx.co.uk>
2023-11-20 00:24:21 +0500

    Update PortfolioItem.js

--- src/data/PortfolioItem.js
+++ src/data/PortfolioItem.js
@@ -6,10 +6,11 @@ export const portfolioData = [
       "International Real Estate Platform which connects buyers and sellers globally.... ",
     techStack: ["REACT.JS", "MUI-V5", "REDUX", "GOOGLE MAPS SDK"],
     type: "react.JS",
+    backgroundImage: "v-tech-background",
     projectDetails: {
-      title: "V-tech",
+      title: "Vital Home Solutions - Sales System For Real Estate Investors",
       detailDiscription:
-        "V-tech is a global platform that facilitates connections between buyers and sellers. It serves as a real estate marketplace where users can search for properties, list their own properties, and engage with fellow users. Additionally, V-tech offers campaigns and strategies for those looking to sell their properties.",
+        "The purpose of Vtech Upgraded is to provide a lead conversion system for scaling real estate leads. Vtech upgraded is focused on managing and tracking different campaigns, leads linked to those campaigns, follow up, offers and appointments of those leads and finally the leads closure or disposal",
       sliderImages: ["vtech1", "vtech2", "vtech3"],
       isCompanyProject: true,
       projectLink: "https://dev.vtech-upgraded.com/",
@@ -27,10 +28,15 @@ export const portfolioData = [
       "WALLET INTEGRATION",
     ],
     type: "react.JS",
+    backgroundImage: "ventiswap-background",
     projectDetails: {
       title: "Ventiswap",
       detailDiscription:
         "Ventiswap is a cryptocurrency exchange and digital wallet that allows users to buy, sell, and store cryptocurrencies. It also offers a variety of other services, such as a crypto wallet, a crypto debit card, and a crypto exchange. Ventiswap is a cryptocurrency exchange and digital wallet that allows users exhange ctyspto currencies.",
+      isCompanyProject: true,
+      projectLink: "https://app.ventiswap.com/",
+      sliderImages: ["ventiswap1", "ventiswap2"],
+      sliderVideos: ["https://www.youtube.com/embed/UDKajZYuzYQ"],
     },
   },
   {
@@ -39,15 +45,18 @@ export const portfolioData = [
     subtitle: "NFT Marketplace for Global Artists & Creators.... ",
     techStack: ["NEXT.JS", "BOOTSTRAP", "MORALIS SDK", "NFT INTEGRATION"],
     type: "react.JS",
+    backgroundImage: "yoor-background",
     projectDetails: {
       title: "Yoor",
       detailDiscription:
-        "Yoor is a marketplace for NFTs (non-fungible tokens) that allows users to buy, sell, and trade digital assets. It also offers a variety of other services, such as a crypto wallet, a crypto debit card, and a crypto exchange. Yoor is a marketplace for NFTs (non-fungible tokens) that allows users to buy, sell, and trade digital assets.",
+        "Yoor is a blockchain-based online platform to sell and buy non-fungible tokens (NFTs). Yoor marketplace is a platform where non-fungible tokens are stored and traded. Similar to other e-commerce websites, they can focus on a specific niche or offer various crypto goods. Digital assets that can be sold there may have a fixed price. Alternatively, users can buy them at an auction.",
+      isCompanyProject: true,
+      projectLink: "https://yoor.art/",
     },
   },
   {
     id: 4,
-    title: "Fomatic",
+    title: "Formatic",
     subtitle: "Dynamic Task Management & Workflow Tool for Teams.... ",
     techStack: [
       "NEXT.JS",
@@ -56,10 +65,17 @@ export const portfolioData = [
       "BOARD INTEGRATION",
     ],
     type: "react.JS",
+    backgroundImage: "formatic-background",
     projectDetails: {
-      title: "Fomatic",
+      title: "Formatic - Cleaver google forms & sheets management",
+      isCompanyProject: true,
+      sliderImages: ["formatic1", "formatic2"],
+      sliderVideos: [
+        "https://www.youtube.com/embed/aWiLk4Uz6fc?si=Jsm2nNKWRlN0HVjq",
+      ],
+      projectLink: "https://app.myformatic.com/",
       detailDiscription:
-        "Fomatic is a dynamic task management and workflow tool for teams. It allows users to create tasks, assign them to team members, and track their progress. Fomatic is a dynamic task management and workflow tool for teams. It allows users to create tasks, assign them to team members, and track their progress.",
+        "Formatic is a CRM solution for form, task and lead management, which can be either installed as an add-on for Google Forms and Sheets or used as stand-alone software. It takes only minutes to master, but it gives you the ability to effectively manage your customer experience, making it an excellent solution for solo entrepreneurs, small businesses, or educators.",
     },
   },
   {
@@ -69,47 +85,54 @@ export const portfolioData = [
       "Comprehensive E-Commerce Solution To Provide A Seamless Shopping Experience.... ",
     techStack: ["REACT.JS", "STRIPE SDK", "EXPRESS.JS", "MONGODB"],
     type: "MERN",
+    backgroundImage: "wilwinhk-background",
     projectDetails: {
       title: "Wilwinhk",
       detailDiscription:
         "Wilwinhk is a comprehensive e-commerce solution that provides a seamless shopping experience. It allows users to browse products, add them to their cart, and checkout with ease. Wilwinhk is a comprehensive e-commerce solution that provides a seamless shopping experience. It allows users to browse products, add them to their cart, and checkout with ease.",
+      sliderImages: ["wilwinhk1", "wilwinhk2", "wilwinhk3"],
+      projectLink: "https://wilwinhk-frontend-app.vercel.app/",
     },
   },
-  {
-    id: 6,
-    title: "Evanglium",
-    subtitle:
-      "Multi-Platform NFT Display & Wallet Integration Platform To Showcase NFTs & Digital Art.... ",
-    techStack: ["NEXT.JS", "MUI-V5", "MORALIS SDK", "ETHER.JS"],
-    type: "react.JS",
-    projectDetails: {
-      title: "Evanglium",
-      detailDiscription:
-        "Evanglium is a multi-platform NFT display and create wallet integration platform that allows users to showcase their NFTs and digital art. It also offers a variety of other services, such as a crypto wallet, a crypto debit card, and a crypto exchange. Evanglium is a multi-platform NFT display and create wallet integration platform that allows users to showcase their NFTs and digital art.",
-    },
-  },
-  {
-    id: 7,
-    title: "SmartNet",
-    subtitle: "Communication & Contact Sharing Platform for Businesses.... ",
-    techStack: ["REACT.JS", "SEMANTIC UI", "TWILIO SDK"],
-    type: "MERN",
-    projectDetails: {
-      title: "SmartNet",
-      detailDiscription:
-        "SmartNet is a communication and contact sharing platform for businesses. It allows users to create groups, send messages, and share contacts with ease. SmartNet is a communication and contact sharing platform for businesses. It allows users to create groups, send messages, and share contacts with ease.",
-    },
-  },
+  // {
+  //   id: 6,
+  //   title: "Evanglium",
+  //   subtitle:
+  //     "Multi-Platform NFT Display & Wallet Integration Platform To Showcase NFTs & Digital Art.... ",
+  //   techStack: ["NEXT.JS", "MUI-V5", "MORALIS SDK", "ETHER.JS"],
+  //   type: "react.JS",
+  //   projectDetails: {
+  //     title: "Evanglium",
+  //     detailDiscription:
+  //       "Evanglium is a multi-platform NFT display and create wallet integration platform that allows users to showcase their NFTs and digital art. It also offers a variety of other services, such as a crypto wallet, a crypto debit card, and a crypto exchange. Evanglium is a multi-platform NFT display and create wallet integration platform that allows users to showcase their NFTs and digital art.",
+  //   },
+  // },
+  // {
+  //   id: 7,
+  //   title: "SmartNet",
+  //   subtitle: "Communication & Contact Sharing Platform for Businesses.... ",
+  //   techStack: ["REACT.JS", "SEMANTIC UI", "TWILIO SDK"],
+  //   type: "MERN",
+  //   projectDetails: {
+  //     title: "SmartNet",
+  //     detailDiscription:
+  //       "SmartNet is a communication and contact sharing platform for businesses. It allows users to create groups, send messages, and share contacts with ease. SmartNet is a communication and contact sharing platform for businesses. It allows users to create groups, send messages, and share contacts with ease.",
+  //   },
+  // },
   {
     id: 9,
     title: "Alnoor Mart",
     subtitle: "Online Shopping Experience for Customers & Vendors.... ",
     techStack: ["WORDPRESS", "ELEMENTOR", "WOOCOMMERCE"],
     type: "wordPress",
+    backgroundImage: "alnoor-mart-background",
     projectDetails: {
       title: "Alnoor Mart",
+      sliderImages: ["alnoor-mart1", "alnoor-mart2", "alnoor-mart3"],
       detailDiscription:
         "Alnoor Mart is an online shopping experience for customers and vendors. It allows users to browse products, add them to their cart, and checkout with ease. Alnoor Mart is an online shopping experience for customers and vendors. It allows users to browse products, add them to their cart, and checkout with ease.",
+      projectLink: "https://alnoormart.com/",
+      isWithCode: true,
     },
   },
   {
@@ -118,10 +141,14 @@ export const portfolioData = [
     subtitle: "Herbal Products  & Supplements to Boost Immunity.... ",
     techStack: ["WORDPRESS", "ELEMENTOR"],
     type: "wordPress",
+    backgroundImage: "alnoor-herbal-background",
     projectDetails: {
       title: "Alnoor Herbal",
       detailDiscription:
         "Alnoor Herbal is a herbal products and supplements to boost immunity. It allows users to browse products, add them to their cart, and checkout with ease. Alnoor Herbal is a herbal products and supplements to boost immunity. It allows users to browse products, add them to their cart, and checkout with ease.",
+      sliderImages: ["alnoor-herbal1", "alnoor-herbal2"],
+      projectLink: "https://alnoorherbal.com/",
+      isWithCode: true,
     },
   },
   {
@@ -130,60 +157,64 @@ export const portfolioData = [
     subtitle: "Real Estate Listings Platform for International Buyers.... ",
     techStack: ["WORDPRESS", "WP-BAKERY", "API INTEGRATION"],
     type: "wordPress",
+    backgroundImage: "alnoor-international-background",
     projectDetails: {
       title: "Alnoor International",
       detailDiscription:
         "Alnoor International is a real estate listings platform for international buyers. It allows users to browse properties, add them to their cart, and checkout with ease. Alnoor International is a real estate listings platform for international buyers. It allows users to browse properties, add them to their cart, and checkout with ease.",
+      sliderImages: ["alnoor-international1"],
+      projectLink: "https://www.alnoorinternationalimpex.com/",
+      isWithCode: true,
     },
   },
-  {
-    id: 12,
-    title: "LMS (Teacher Evaluation System Module)",
-    subtitle:
-      "Educational Management & Evaluation System for Teachers, Students & Admin.... ",
-    techStack: ["REACT.JS", "MUI-V4", "REDUX"],
-    type: "react.JS",
-    projectDetails: {
-      title: "LMS (Teacher Evaluation System Module)",
-      detailDiscription:
-        "LMS (Teacher Evaluation System Module) is an educational management and evaluation system for teachers, students, and admin. It allows users to create groups, send messages, and share contacts with ease. LMS (Teacher Evaluation System Module) is an educational management and evaluation system for teachers, students, and admin. It allows users to create groups, send messages, and share contacts with ease.",
-    },
-  },
-  {
-    id: 13,
-    title: "Small Survival",
-    subtitle:
-      "Online Course Directory for Students & Teachers to earn money.... ",
-    techStack: ["NEXT.JS", "SEMANTIC-UI", "REDUX"],
-    type: "react.JS",
-    projectDetails: {
-      title: "Small Survival",
-      detailDiscription:
-        "Small Survival is an online course directory for students and teachers to earn money. It allows users to browse courses, add them to their cart, and checkout with ease. Small Survival is an online course directory for students and teachers to earn money. It allows users to browse courses, add them to their cart, and checkout with ease.Its still in development phase.",
-    },
-  },
-  {
-    id: 14,
-    title: "Tech Survival",
-    subtitle: "Global & Local Tech News Portal for Tech Enthusiasts.... ",
-    techStack: ["NEXT.JS", "SEMANTIC-UI", "RAPID API SDK"],
-    type: "react.JS",
-    projectDetails: {
-      title: "Tech Survival",
-      detailDiscription:
-        "Tech Survival is a global and local tech news portal for tech enthusiasts. It allows users to browse news, add them to their cart, and checkout with ease. Tech Survival is a global and local tech news portal for tech enthusiasts. It allows users to browse news, add them to their cart, and checkout with ease.",
-    },
-  },
-  {
-    id: 15,
-    title: "Quiz App",
-    subtitle: "Interactive Quiz Application for Students.... ",
-    techStack: ["HTML", "CSS", "JS"],
-    type: "withoutFramework",
-    projectDetails: {
-      title: "Quiz App",
-      detailDiscription:
-        "Quiz App is an interactive quiz application for students. It allows users to browse questions, add them to their cart, and checkout with ease. Quiz App is an interactive quiz application for students. It allows users to browse questions, add them to their cart, and checkout with ease.",
-    },
-  },
+  // {
+  //   id: 12,
+  //   title: "LMS (Teacher Evaluation System Module)",
+  //   subtitle:
+  //     "Educational Management & Evaluation System for Teachers, Students & Admin.... ",
+  //   techStack: ["REACT.JS", "MUI-V4", "REDUX"],
+  //   type: "react.JS",
+  //   projectDetails: {
+  //     title: "LMS (Teacher Evaluation System Module)",
+  //     detailDiscription:
+  //       "LMS (Teacher Evaluation System Module) is an educational management and evaluation system for teachers, students, and admin. It allows users to create groups, send messages, and share contacts with ease. LMS (Teacher Evaluation System Module) is an educational management and evaluation system for teachers, students, and admin. It allows users to create groups, send messages, and share contacts with ease.",
+  //   },
+  // },
+  // {
+  //   id: 13,
+  //   title: "Small Survival",
+  //   subtitle:
+  //     "Online Course Directory for Students & Teachers to earn money.... ",
+  //   techStack: ["NEXT.JS", "SEMANTIC-UI", "REDUX"],
+  //   type: "react.JS",
+  //   projectDetails: {
+  //     title: "Small Survival",
+  //     detailDiscription:
+  //       "Small Survival is an online course directory for students and teachers to earn money. It allows users to browse courses, add them to their cart, and checkout with ease. Small Survival is an online course directory for students and teachers to earn money. It allows users to browse courses, add them to their cart, and checkout with ease.Its still in development phase.",
+  //   },
+  // },
+  // {
+  //   id: 14,
+  //   title: "Tech Survival",
+  //   subtitle: "Global & Local Tech News Portal for Tech Enthusiasts.... ",
+  //   techStack: ["NEXT.JS", "SEMANTIC-UI", "RAPID API SDK"],
+  //   type: "react.JS",
+  //   projectDetails: {
+  //     title: "Tech Survival",
+  //     detailDiscription:
+  //       "Tech Survival is a global and local tech news portal for tech enthusiasts. It allows users to browse news, add them to their cart, and checkout with ease. Tech Survival is a global and local tech news portal for tech enthusiasts. It allows users to browse news, add them to their cart, and checkout with ease.",
+  //   },
+  // },
+  // {
+  //   id: 15,
+  //   title: "Quiz App",
+  //   subtitle: "Interactive Quiz Application for Students.... ",
+  //   techStack: ["HTML", "CSS", "JS"],
+  //   type: "withoutFramework",
+  //   projectDetails: {
+  //     title: "Quiz App",
+  //     detailDiscription:
+  //       "Quiz App is an interactive quiz application for students. It allows users to browse questions, add them to their cart, and checkout with ease. Quiz App is an interactive quiz application for students. It allows users to browse questions, add them to their cart, and checkout with ease.",
+  //   },
+  // },
 ];
