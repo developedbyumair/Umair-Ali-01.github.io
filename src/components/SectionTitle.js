cdd6943bb4e9c7038ef61ad397647d0ea388d7cc
Umair Ali <umairsadaqat@invotyx.co.uk>
2024-02-14 00:22:28 +0500

    Update SectionTitle.js

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -1,14 +1,22 @@
 const SectionTitle = ({ heading, subHeading, text }) => {
   return (
     <div className="row section-heading">
-      <div className="col-lg-6">
+      <div className="col-lg-10">
         <h6>
           <span>{subHeading}</span>
         </h6>
         <h3>
           <span>{heading}</span>
         </h3>
-        {text && <p>{text}</p>}
+        {text && (
+          <p
+            style={{
+              whiteSpace: "pre-line",
+            }}
+          >
+            {text}
+          </p>
+        )}
       </div>
     </div>
   );
