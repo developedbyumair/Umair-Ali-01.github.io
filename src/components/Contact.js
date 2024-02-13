779dd5d812f4539fbb380aa604fa1017362e8280
Umair Ali <umairsadaqat@invotyx.co.uk>
2024-02-14 00:14:59 +0500

    feat: Contact Us

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import emailjs from "emailjs-com";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
 import SectionTitle from "./SectionTitle";
 
 const contactInfo = [
@@ -28,8 +29,129 @@ const Contact = () => {
   const { name, email, subject, message } = mailData;
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
   const onChange = (e) => {
     setMailData({ ...mailData, [e.target.name]: e.target.value });
+  };
+  const buttonRef = useRef(null);
+  const confettiAmount = 100;
+  const confettiColors = [
+    "#7d32f5",
+    "#f6e434",
+    "#63fdf1",
+    "#e672da",
+    "#295dfe",
+    "#6e57ff",
+  ];
+
+  const random = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  const createConfetti = (to) => {
+    let elem = document.createElement("i"),
+      set = Math.random() < 0.5 ? -1 : 1;
+    elem.style.setProperty("--x", random(-360, 360) + "px");
+    elem.style.setProperty("--y", random(-200, 200) + "px");
+    elem.style.setProperty("--r", random(0, 360) + "deg");
+    elem.style.setProperty("--s", random(0.6, 1));
+    elem.style.setProperty("--b", confettiColors[random(0, 5)]);
+    to.appendChild(elem);
+  };
+  const handleClickEvent = (e) => {
+    const button = buttonRef.current;
+    let complete = false,
+      up = 10;
+    const timeline = gsap.timeline({
+      paused: true,
+      ease: "none",
+      onComplete() {
+        complete = true;
+        button.classList.add("complete");
+        for (let i = 0; i < confettiAmount; i++) {
+          createConfetti(button);
+        }
+        setTimeout(() => {
+          button.classList.add("confetti");
+          setTimeout(
+            () => button.querySelectorAll("i").forEach((i) => i.remove()),
+            600
+          );
+        }, 300);
+        // Reset
+        setTimeout(() => {
+          button.classList.remove("complete", "confetti");
+          complete = false;
+        }, 2000);
+      },
+    });
+
+    timeline.to(button, {
+      keyframes: [
+        {
+          "--weight-y": -6,
+          "--arm-rotate-s-x": 90,
+          duration: 0.3,
+        },
+        {
+          "--weight-y": -10,
+          "--arm-rotate-s-x": 45,
+          "--arm-rotate-s": 130,
+          duration: 0.2,
+        },
+        {
+          "--weight-y": -12,
+          "--arm-rotate-s": 130,
+          "--arm-rotate-s-x": 0,
+          duration: 0.1,
+        },
+        {
+          "--weight-y": -20,
+          "--person-y": -4,
+          "--arm-rotate": 100,
+          "--arm-rotate-s": 90,
+          "--leg-y": 0,
+          "--leg-rotate": 20,
+          "--leg-rotate-s": -20,
+          duration: 0.2,
+        },
+        {
+          "--weight-y": -25,
+          "--arm-rotate": 150,
+          "--arm-rotate-s": 30,
+          duration: 0.2,
+        },
+      ],
+    });
+
+    up = 10;
+
+    const rippleDiv = document.createElement("div");
+
+    rippleDiv.className = "ripple";
+
+    const boundingClientRect = button.getBoundingClientRect();
+
+    button.style.setProperty("--ripple-x", e.clientX - boundingClientRect.left);
+    button.style.setProperty("--ripple-y", e.clientY - boundingClientRect.top);
+
+    button.querySelector(".inner").appendChild(rippleDiv);
+
+    setTimeout(() => rippleDiv.remove(), 500);
+
+    const interval = setInterval(() => {
+      up = up > 0 ? up - 0.1 : 0;
+
+      let progress = timeline.progress(),
+        direction = up > 0 ? 1 : -1.5;
+
+      if (!complete) {
+        timeline.progress(progress + 0.01 * direction);
+      }
+    }, 1000 / 60);
+
+    return () => {
+      clearInterval(interval);
+    };
   };
   const onSubmit = (e) => {
     e.preventDefault();
@@ -41,6 +163,7 @@ const Contact = () => {
     ) {
       setError(true);
     } else {
+      handleClickEvent(e);
       emailjs
         .send(
           "service_9u9gdab", // service id
@@ -70,7 +193,10 @@ const Contact = () => {
         <SectionTitle
           heading={"Let's Discuss Project"}
           subHeading={"Contact"}
+          text={`Looking for someone to sprinkle creativity on your projects?
+           I am here your friendly neighborhood Product Designer`}
         />
+
         <div className="row">
           <div className="col-lg-6">
             <div className="contact-form">
@@ -114,13 +240,13 @@ const Contact = () => {
                   </div>
                   <div className="col-12">
                     <div className="form-group">
-                      <label className="form-label">Subject</label>
+                      <label className="form-label">Project Idea</label>
                       <input
                         name="subject"
                         onChange={(e) => onChange(e)}
                         value={subject}
                         id="subject"
-                        placeholder="Subject *"
+                        placeholder="Project Idea *"
                         className={`form-control ${
                           error ? (subject.length !== 0 ? "" : "invalid") : ""
                         }`}
@@ -136,8 +262,8 @@ const Contact = () => {
                         onChange={(e) => onChange(e)}
                         value={message}
                         id="message"
-                        placeholder="Details *"
-                        rows={3}
+                        placeholder="Whether it's short-term projects or long-term partnerships, I'm here to support your design needs. Reach out for a virtual coffee chat or to explore how we can create magic together! 🌟 *"
+                        rows={4}
                         className={`form-control ${
                           error ? (message.length !== 0 ? "" : "invalid") : ""
                         }`}
@@ -153,13 +279,22 @@ const Contact = () => {
                   </div>
                   <div className="col-md-12">
                     <div className="send">
-                      <button
-                        className="px-btn px-btn-theme2"
-                        type="submit"
-                        value="Send"
-                      >
-                        {" "}
-                        Send Message
+                      <button ref={buttonRef} className="button2" type="submit">
+                        <div className="inner">
+                          <div className="icon">
+                            <div className="person">
+                              <div className="arm"></div>
+                              <div className="arm right"></div>
+                              <div className="leg"></div>
+                              <div className="leg right"></div>
+                            </div>
+                            <div className="weight"></div>
+                          </div>
+                          <div className="text">
+                            <span>Smash to submit</span>
+                            <span>Yaay! Submitted.</span>
+                          </div>
+                        </div>
                       </button>
                     </div>
                   </div>
@@ -188,7 +323,7 @@ const Contact = () => {
               ))}
             </ul>
             <div className="text-center pt-5">
-              <img src="assets/img/contact.svg" className="svg" alt="image" />
+              <img src="assets/img/Croods1.svg" className="svg" alt="image" />
             </div>
           </div>
         </div>
