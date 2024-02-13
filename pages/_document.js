dae3d2286b1f29d1b9aa3f9c5b3c1af9b85098ad
Umair Ali <umairsadaqat@invotyx.co.uk>
2024-02-14 00:46:29 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -12,11 +12,8 @@ export default function Document() {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <meta name="keywords" content="Umair - Portfolio" />
-        <meta name="description" content="Next JS Developer Portfolio" />
-        <meta
-          property="og:image"
-          content="https://www.w3schools.com/images/w3schools_logo_436_2.png"
-        />
+        <meta property="og:description" content="Next JS Developer Portfolio" />
+        <meta property="og:image" content="/assets/img/Banner.png" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="436" />
         <meta property="og:image:height" content="228" />
