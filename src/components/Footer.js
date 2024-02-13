29b906f4646c53a0fc6f698a6945b11576d17ac5
Umair Ali <umairsadaqat@invotyx.co.uk>
2024-02-13 23:21:28 +0500

    Feat Footer Animation [no ci]

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,26 @@
 const Footer = () => {
   return (
-    <footer className="footer">
+    <div className="footer">
+      <footer className="new_footer_area bg_color">
+        <div className="new_footer_top">
+          <div className="container"></div>
+          <div className="footer_bg">
+            <div className="footer_bg_one"></div>
+            <div className="footer_bg_two"></div>
+          </div>
+        </div>
+      </footer>
       <div className="container">
         <div className="row align-items-center">
+          <div className="col-md-6 py-2 text-center text-md-end">
+            <p className="m-0  fw-bold">Feel free to connect with me on</p>
+          </div>
           <div className="col-md-6 py-2">
             <div className="nav justify-content-center justify-content-md-start">
               <a href="https://github.com/developedbyumair" target="_blank">
                 <i className="fab fa-github" />
               </a>
-              <a href="https://twitter.com/DevByUmair" target="_blank">
+              <a href="https://twitter.com/hyeumair" target="_blank">
                 <i className="fab fa-twitter" />
               </a>
               <a href="https://www.linkedin.com/in/umairali5/" target="_blank">
@@ -16,14 +28,9 @@ const Footer = () => {
               </a>
             </div>
           </div>
-          <div className="col-md-6 py-2 text-center text-md-end">
-            <p className="m-0">
-              © {new Date().getFullYear()} copyright all right reserved
-            </p>
-          </div>
         </div>
       </div>
-    </footer>
+    </div>
   );
 };
 export default Footer;
